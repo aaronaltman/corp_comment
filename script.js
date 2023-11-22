d1ffa5fb97df1abd854f5e101495cbb7c81e3726
aka <aaronkaltman@gmail.com>
2023-11-22 06:36:06 -0500

    adding list item html

--- script.js
+++ script.js
@@ -36,7 +36,37 @@ const buttonHandler = (event) => {
     setTimeout(() => {
       formEl.classList.remove("form--invalid");
     }, 2000);
+    return;
   }
+  // Get info to Create new list item --
+  const hashtag = inputValue
+    .split(" ")
+    .find((word) => word.includes("#"))
+    .replace("#", "");
+  const company = hashtag.split(0);
+  const badgeLetter = hashtag.substring(0, 1).toUpperCase();
+  const upVote = 0;
+  const daysAgo = 0;
+
+  // Create new list item
+  const feedbackItemHTML = `
+ <li class="feedback">
+    <button class="upvote">
+        <i class="fa-solid fa-caret-up upvote__icon"></i>
+        <span class="upvote__count">${upVote}</span>
+    </button>
+    <section class="feedback__badge">
+        <p class="feedback__letter">${badgeLetter}</p>
+    </section>
+    <div class="feedback__content">
+        <p class="feedback__company">${company}</p>
+        <p class="feedback__text">${inputValue}</p>
+    </div>
+    <p class="feedback__date">${daysAgo}</p>
+</li>
+  
+  `;
+  listEl.innerHTML = feedbackItemHTML;
 };
 
 formEl.addEventListener("submit", buttonHandler);
