81df00e2e12b0587ada7b9940f1a0bdbc85697c1
aka <aaronkaltman@gmail.com>
2023-11-22 15:52:12 -0500

    iterating over feedback and adding to list

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Global variables
+const MAX_CHARS = 150;
+
 const buttonEl = document.querySelector(".submit-btn");
 const inputEl = document.querySelector(".form__textarea");
 const listEl = document.querySelector(".feedbacks");
@@ -6,10 +8,10 @@ const formEl = document.querySelector(".form");
 const textareaEl = document.querySelector(".form__textarea");
 const counterEl = document.querySelector(".counter");
 
-// Create a new list item --- inputHandler
+// Input Handler -- Counter -- 
 const inputHandler = () => {
   // Determine maximum number of characters
-  const maxCharacters = 150;
+  const maxCharacters = MAX_CHARS;
   // Determine number of characters in textarea
   const currentCharacters = textareaEl.value.length;
   // Determine number of characters remaining
@@ -20,24 +22,27 @@ const inputHandler = () => {
 
 textareaEl.addEventListener("input", inputHandler);
 
-// Create a new list item -- buttonHandler
+// Form Handler --
+const formValidation = textCheck => {
+  const className = textCheck === 'valid' ? 'form--valid' : 'form--invalid';
+
+  formEl.classList.add(className);
+    setTimeout(() => {
+      formEl.classList.remove(className);
+    }, 2000);
+
+}
 const buttonHandler = (event) => {
   // Prevent default behavior
   event.preventDefault();
   const inputValue = inputEl.value;
 
   if (inputValue.includes("#") && inputValue.length >= 5) {
-    formEl.classList.add("form--valid");
-    setTimeout(() => {
-      formEl.classList.remove("form--valid");
-    }, 2000);
+    formValidation('valid');
   } else {
-    formEl.classList.add("form--invalid");
-    setTimeout(() => {
-      formEl.classList.remove("form--invalid");
-    }, 2000);
-    return;
+    formValidation('invalid');
   }
+
   // Get info to Create new list item --
   const hashtag = inputValue
     .split(" ")
@@ -66,6 +71,42 @@ const buttonHandler = (event) => {
 </li>
   `;
   listEl.insertAdjacentHTML("beforeend", feedbackItemHTML);
+
+  // Clear textarea
+  inputEl.value = "";
+  // Reset counter
+  counterEl.textContent = MAX_CHARS;
+  // Reset button
+  buttonEl.blur() = true;
 };
 
 formEl.addEventListener("submit", buttonHandler);
+
+
+// Feedback List Component --
+fetch('https://bytegrad.com/course-assets/js/1/api/feedbacks')
+.then(response => response.json())
+.then(data => {
+// iterate over data.feedbacks
+data.feedbacks.forEach(feedbackItem => {
+  // Create new list item
+  const feedbackItemHTML = `
+ <li class="feedback">
+    <button class="upvote">
+        <i class="fa-solid fa-caret-up upvote__icon"></i>
+        <span class="upvote__count">${feedbackItem.upvoteCount}</span>
+    </button>
+    <section class="feedback__badge">
+        <p class="feedback__letter">${feedbackItem.badgeLetter}</p>
+    </section>
+    <div class="feedback__content">
+        <p class="feedback__company">${feedbackItem.company}</p>
+        <p class="feedback__text">${feedbackItem.text}</p>
+    </div>
+    <p class="feedback__date">${feedbackItem.daysAgo === 0 ? "NEW" : `${feedbackItem.daysAgo}d`}</p>
+</li>
+  `;
+
+  listEl.insertAdjacentHTML("beforeend", feedbackItemHTML);
+  });
+});
