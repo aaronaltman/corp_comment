e5b676047049bdaf74e5b8bd181674db7b2724ac
aka <aaronkaltman@gmail.com>
2023-11-28 15:15:51 -0500

    when submitted console logs text

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-// Global Variables
+// -- Global Variables --
 const textAreaEl = document.querySelector(".form__textarea");
 const formEl = document.querySelector(".form");
 const submitButtonEl = document.querySelector(".submit-btn");
 const counterEl = document.querySelector(".counter");
 MAX_CHARS = 150;
 
-// Counter Component
-// InputHandler function
+// Counter Component - counts the number of characters in the text area
 const inputHandler = () => {
   const text = textAreaEl.value;
   const textLength = text.length;
-  let charsLeft = MAX_CHARS - textLength;
-  // Counter needs to count down as characters are added
+  const charsLeft = MAX_CHARS - textLength;
   counterEl.textContent = charsLeft;
 };
 // input event listener - then execute the inputHandler function
@@ -20,7 +18,8 @@ textAreaEl.addEventListener("input", inputHandler);
 // Submit Button
 const submitHandler = (event) => {
   event.preventDefault();
-  console.log("submitted");
+  const text = textAreaEl.value;
+  console.log(text);
 };
 // when clicked: log the input, store the input in a variable, and count the length of the input
 formEl.addEventListener("submit", submitHandler);
@@ -28,3 +27,5 @@ formEl.addEventListener("submit", submitHandler);
 // Log the data to the server
 
 // Retrieve the data from the server
+
+// Display the data on the page
